8d1b180a3f9605ce6648970d24b4415ca5195ba7
voytenko.d <voytenko.d@ideasoft.io>
2019-09-25 16:02:25 +0300

    Created form to save links in database

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    layoutsDir: path.join(app.get('views'), 'partials'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
 }));
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-require('../database');
+const pool = require('../database');
+
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink])
+    res.send('reseived')
+})
 
 module.exports = router;
